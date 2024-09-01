1ed7f072c28d4dc4fe556dd612182a6301e16ed1
Dan Whitehouse <dan.whitehouse@tbaplc.co.uk>
2024-09-01 19:09:56 +0100

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+import  {Client, Databases} from 'node-appwrite'
+
+///Enviroment variables
+const PROJECT_ID = process.env.PROJECT_ID
+const DB_ID = process.env.DB_ID
+const COLLECTION_ID_CONNECTIONS = process.env.COLLECTION_ID_CONNECTIONS
+
+
+////Function
 export default async ({req, res, log, error})=>{
+    const client = new Client ()
+    client
+    .setEndpoint ('https://cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID)
+
+    const db = new Databases(client)
+
+    if(req.method == 'GET'){
+        const response = await db.listDocuments(
+            DB_ID,
+            COLLECTION_ID_CONNECTIONS
+        )
+        return res.json(response.documents)
+    }
     return res.send('Hello World')
 }
