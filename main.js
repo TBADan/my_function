e407d8f8ffcfbaec81df4b0c3b43e79d5a749ad5
Dan Whitehouse <dan.whitehouse@tbaplc.co.uk>
2024-09-30 19:29:35 +0100

    --

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ export default async ({ req, res, log, error }) => {
 
   const db = new Databases(client);
 
-  if (req.method === 'GET') {
+  if (req.method === 'POST') {
     try {
       const response = await db.listDocuments(
         DB_ID,
